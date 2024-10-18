ddfe5d0d81b070503207bb8517a75ef6f0974c52
guinea-big <khantuevandrei@gmail.com>
2024-10-18 10:38:45 +0300

    Create func playRound that takes human & computer choices, calculates result & outputs it to console

--- script.js
+++ script.js
@@ -28,3 +28,43 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+
+    switch (computerChoice) {
+        case 'rock':
+            if (humanChoice === 'rock') {
+                console.log('Draw! Rock vs Rock');
+            } else if (humanChoice === 'paper') {
+                humanScore++;
+                console.log('You win! Rock beats Paper');
+            } else {
+                computerScore++;
+                console.log('You lose! Rock beats Scissors');
+            }
+            break;
+        case 'paper':
+            if (humanChoice === 'rock') {
+                computerScore++;
+                console.log('You lose! Paper beats Rock');
+            } else if (humanChoice === 'paper') {
+                console.log('Draw! Paper vs Paper');
+            } else {
+                humanScore++;
+                console.log('You win! Scissors beat Paper');
+            }
+            break;
+        case 'scissors':
+            if (humanChoice === 'rock') {
+                humanScore++;
+                console.log('You win! Rock beats Scissors');
+            } else if (humanChoice === 'paper') {
+                computerScore++;
+                console.log('You lose! Scissors beat Paper');
+            } else {
+                console.log('Draw! Scissors vs Scissors');
+            }
+    }
+}
