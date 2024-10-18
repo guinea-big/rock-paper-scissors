578f35e7a75cbf05e56138406f286df173ed49be
guinea-big <khantuevandrei@gmail.com>
2024-10-18 09:39:07 +0300

    Modify getComputerChoice so it returns a string with one of 3 results (rock, paper, scissors)

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3) +1 
+    const randomNumber = Math.floor(Math.random() * 3) +1;
+    
+    let result;
+
+    switch (randomNumber) {
+        case 1:
+            result = 'rock';
+            break;
+        case 2:
+            result = 'paper';
+            break;
+        case 3:
+            result = 'scissors';
+            break;
+        default:
+            console.log('Something went wrong');
+    }
+    
+    return result;
 }
+
